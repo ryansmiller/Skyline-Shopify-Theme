8ead4bc4083c25fd8d2c3338c0025eeac8de5cc0
ryansmiller <ryansmiller@gmail.com>
2025-02-16 21:19:25 -0500

    change the form append

--- assets/product-form.js
+++ assets/product-form.js
@@ -32,14 +32,16 @@ if (!customElements.get('product-form')) {
         delete config.headers['Content-Type'];
 
         const formData = new FormData(this.form);
-        if (this.cart) {
-          formData.append(
-            'sections',
-            this.cart.getSectionsToRender().map((section) => section.id)
-          );
-          formData.append('sections_url', window.location.pathname);
-          this.cart.setActiveElement(document.activeElement);
-        }
+
+if (this.cart && typeof this.cart.getSectionsToRender === 'function') {
+  formData.append(
+    'sections',
+    this.cart.getSectionsToRender().map(section => section.id)
+  );
+  formData.append('sections_url', window.location.pathname);
+  this.cart.setActiveElement(document.activeElement);
+}
+
         config.body = formData;
 
         fetch(`${routes.cart_add_url}`, config)
